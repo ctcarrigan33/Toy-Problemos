bc9bcae3b7254070e825135c5b16afd03e0ca2b1
Charlie Carrigan <ctcarrigan33@gmail.com>
2016-10-03 12:24:20 -0500

    arrayception started

---
+++ arrayception/arrayception.js
@@ -0,0 +1,10 @@
+// Given an array of arbitrarily nested arrays, return n, where n is
+// the deepest level that contains a non-array value.
+//
+// Examples
+// Input	                                          Output
+// array: [ [ 5 ], [ [ ] ] ]	                  |      2
+// array: [ 10, 20, 30, 40 ]	                  |      1
+// array: [ [ 10, 20 ], [ [ 30, [ 40 ] ] ] ]	  |      4
+// array: [ ]	                                |      0
+// array: [ [ [ ] ] ]	                        |      0
